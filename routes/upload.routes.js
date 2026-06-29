29e635ce428a4f94518933a3929f02f37f556bc8
Sima-akhter <simaakter5301@gmail.com>
2026-06-29 12:34:27 +0600

    fix: make image uploads public to enable profile photo uploads during registration

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const uploadController = require('../controllers/upload.controller');
-const { verifyToken } = require('../middlewares/auth.middleware');
 
-router.post('/image', verifyToken, uploadController.uploadImage);
+router.post('/image', uploadController.uploadImage);
 
 module.exports = router;
